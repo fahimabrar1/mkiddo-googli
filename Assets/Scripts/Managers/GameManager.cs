6e49661957e87bd3ff8192e3ede1e1f9b2cb23cf
Fahim Abrar <fahim.abrar101@gmail.com>
2024-04-17 12:25:34 +0600

    New UI with safe Area, with Build test v1.0.1

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
